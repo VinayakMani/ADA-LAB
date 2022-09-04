cad97c44ff5fec63b22b10dcea0507f151e1f783
Vinayak Mani (202132) <89988949+VinayakMani@users.noreply.github.com>
2022-09-04 23:59:33 +0545

    Add files via upload

---
+++ al -3.py
@@ -0,0 +1,47 @@
+# MergeSort 
+def mergeSort(array):
+    if len(array) > 1:
+
+        #  r is the point where the array is divided into two subarrays
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
+
+        # Sort the two halves
+        mergeSort(L)
+        mergeSort(M)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        # When we run out of elements in either L or M,
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+
+# Print the array
+def printList(array):
+    for i in range(len(array)):
+        print(array[i], end=" ")
+    print()
+
+# Driver program
+if __name__ == '__main__':
+    array = [60, 51, 12, 10, 19, 31]
+    mergeSort(array)
+    print("Sorted array is: ")
+    printList(array)
